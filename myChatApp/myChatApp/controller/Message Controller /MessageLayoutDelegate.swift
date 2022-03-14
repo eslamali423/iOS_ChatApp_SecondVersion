03cc37685b705580fba87091f95b5ebf1342d6d1
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-03-15 00:29:23 +0200

    set heights for bottom and top message cell labels

--- myChatApp/myChatApp/controller/Message Controller /MessageLayoutDelegate.swift
+++ myChatApp/myChatApp/controller/Message Controller /MessageLayoutDelegate.swift
@@ -8,6 +8,49 @@
 import Foundation
 import MessageKit
 
-extension MessagesViewController : MessagesLayoutDelegate {
+extension MSGViewController : MessagesLayoutDelegate {
+    
+    // this function to set the height for lables (section) [date, pull for more messages]
+    // cell top label
+    public func cellTopLabelHeight(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGFloat {
+        
+        if indexPath.section % 3 == 0 {
+            // set height for pull for new messages label
+        }
+   return 10
+    }
+    
+    
+    
+    // this function to set the height for lables (section) [date, pull for more messages]
+    // cell top label
+    public func cellBottomLabelHeight(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGFloat {
+     
+        return isFromCurrentSender(message: message) && indexPath.section == mkMessages.count - 1 ? 17 : 0
+        
+    }
+    
+    public func messageBottomLabelHeight(for message: MessageType, at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> CGFloat {
+  
+        return indexPath.section != mkMessages.count - 1 ? 10 : 0
+   
+    }
+    
+    
+    // i write this function here because isFormCurrentSender doesn't exist here
+// this function from the internet
+//    func isFromCurrentSender(message: MessageType) -> Bool {
+//       
+//        return message.sender.senderId == User.currentID
+//    }
+//
+
+   
+  
+ 
+   
+    
+    
+    
     
 }
