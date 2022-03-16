6fa064eae4a513b85ff5c14b69b30a6868c72399
Eslam Ali <38584724+eslamali423@users.noreply.github.com>
2022-03-16 19:42:37 +0200

    Create Images and Videos Delegate

--- myChatApp/myChatApp/controller/Message Controller /MessageCellDelegate.swift
+++ myChatApp/myChatApp/controller/Message Controller /MessageCellDelegate.swift
@@ -7,8 +7,56 @@
 
 import Foundation
 import MessageKit
+import AVFoundation
+import AVKit
+import SKPhotoBrowser
+
 
 extension MSGViewController : MessageCellDelegate {
+    
+    
+    func didTapImage(in cell: MessageCollectionViewCell) {
+    
+        if let indextPath = messagesCollectionView.indexPath(for: cell) {
+      
+            let mkMessage =  mkMessages[indextPath.section]
+            // check if the message is photo
+            if mkMessage.photoItem != nil && mkMessage.photoItem?.image != nil {
+               var images = [SKPhoto]()
+                let photo = SKPhoto.photoWithImage(mkMessage.photoItem!.image!)
+                images.append(photo)
+                
+                let photoBrowser = SKPhotoBrowser(photos: images)
+                
+                self.present(photoBrowser, animated: true, completion: nil)
+                
+            }
+            // check if the message is video
+            if mkMessage.videoItem != nil && mkMessage.videoItem?.url != nil {
+              
+                let playerController = AVPlayerViewController()
+                let player = AVPlayer(url: mkMessage.videoItem!.url!)
+                playerController.player = player
+                
+                let session  =  AVAudioSession.sharedInstance()
+                try! session.setCategory(.playAndRecord, mode: .default, options: .defaultToSpeaker)
+                
+                self.present(playerController, animated: true) {
+                    playerController.player!.play()
+                }
+                
+                
+                
+                
+                
+            }
+            
+            
+        
+        }
+    }
+    
+    
     
 }
 
